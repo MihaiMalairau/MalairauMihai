39fdde447157b20e444de15474f888dc29a2b563
MalairauMihai <malairau.mihail@isa.utm.md>
2020-09-24 17:53:31 +0300

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+//import java.lang.String;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println(" Introduceti un numar integer : ");
+        Scanner num1 = new Scanner(System.in);
+        int i1;
+        float i2,i3;
+        i1 = num1.nextInt();
+        System.out.println(" Introduceti un numar float : ");
+        Scanner num2 = new Scanner(System.in);
+        i2 = num2.nextFloat();
+        i3 = i2 + i1;
+        System.out.println("Suma este: " + i3 );
+
+    }
+}
